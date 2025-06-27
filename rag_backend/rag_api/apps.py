9af1c410fc53f721300aab9c4d7707b40b1a68b4
Saurav <sauravchaudhary3105@gmail.com>
2025-06-27 23:01:26 +0530

    Delete rag_backend directory

--- rag_backend/rag_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RagApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'rag_api'
--- rag_backend/rag_api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('ask/', rag_query_view),
-    path('index/', index_video_view)
-]
