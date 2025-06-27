7c5c4fb97e2ebdef1ae2b60512e5fd523c8a582d
Saurav <sauravchaudhary3105@gmail.com>
2025-06-27 23:01:37 +0530

    Delete rag_frontend directory

--- rag_frontend/src/components/LoaderSpinner.jsx
+++
@@ -1,8 +0,0 @@
-export default function LoaderSpinner() {
-  return (
-    <div className="text-center mt-4">
-      <div className="inline-block w-6 h-6 border-4 border-green-600 border-dashed rounded-full animate-spin"></div>
-      <p className="text-sm text-gray-600 mt-2">Processing...</p>
-    </div>
-  );
-}
